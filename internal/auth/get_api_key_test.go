cd2fad0fbfdebccd8a8a954cf7cf633c603c44aa
Shaked Tayeb <dokuroc@gmail.com>
2026-07-12 05:50:35 +0300

    fix: wip

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -38,6 +38,7 @@ func TestGetAPIKey(t *testing.T) {
 
 			gotKey, err := GetAPIKey(headers)
 
+			t.Errof("intintonal fiail")
 			if gotKey != tt.wantKey {
 				t.Errorf("GetAPIKey() key = %q, want %q", gotKey, tt.wantKey)
 			}
